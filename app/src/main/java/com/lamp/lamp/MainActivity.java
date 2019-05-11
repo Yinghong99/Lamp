0132bfe0891633c53ba0cf841f08ccea6390d587
Yinghong99 <39016705+Yinghong99@users.noreply.github.com>
2019-05-11 14:11:06 -0700

    cc

--- app/src/main/java/com/lamp/lamp/MainActivity.java
+++ app/src/main/java/com/lamp/lamp/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Log.d("dd", "ww");
         Log.d("dd","ww");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
